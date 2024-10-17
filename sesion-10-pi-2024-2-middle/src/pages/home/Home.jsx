f03d1d4da0b5f7ae6e8f16c29cbd3e8e8fd4a7c3
Kevin <kevinJ1910>
2024-10-16 23:39:46 -0500

    Se integro el sonido al ingresar a la aplicacion

---
+++ sesion-10-pi-2024-2-middle/src/pages/home/Home.jsx
@@ -0,0 +1,56 @@
+import { Canvas } from "@react-three/fiber";
+import Controls from "./controls/Controls";
+import Lights from "./lights/Lights";
+import { Physics } from "@react-three/rapier";
+import Beach from "./world/Beach";
+import Staging from "./staging/Staging";
+import { Loader, PositionalAudio } from "@react-three/drei";
+import { Perf } from "r3f-perf";
+import { Suspense, useCallback, useRef, useState } from "react";
+import Video from "./world/Video";
+
+const Home = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const cameraSettings = {
+    position: [0, 15, 15],
+  };
+
+  const audioRef = useRef(null);
+
+  // Función para manejar el movimiento del ratón y simular reproducir el audio al iniciar la aplicacion
+  const handlePointerMove = useCallback(() => {
+    if (!isPlaying && audioRef.current) {
+      audioRef.current.play();
+      audioRef.current.setVolume(10);
+      setIsPlaying(true); // Marcar como que el audio ya está reproduciéndose
+    }
+  }, [isPlaying]);
+
+  return (
+    <>
+      <Canvas camera={cameraSettings} onPointerMove={handlePointerMove}>
+        <Suspense fallback={null}>
+          <Perf position={"top-left"} />
+          <Controls />
+          <Lights />
+          <Staging />
+          <Physics debug={false}>
+            <Beach />
+          </Physics>
+          <Video name="screen" position-y={10} scale={8} />
+          <group position={[0, 5, 0]}>
+            <PositionalAudio
+              ref={audioRef}
+              url="/sounds/waves.mp3"
+              distance={3}
+              loop
+            />
+          </group>
+        </Suspense>
+      </Canvas>
+      <Loader />
+    </>
+  );
+};
+
+export default Home;
